9ef93d0668db54a9f1041f88c1eb23214952b627
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-12-28 20:01:10 +0100

    hw-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,59 @@
-import Contact from "./components/Contact/Contact.jsx";
-import ContactForm from "./components/ContactForm/ContactForm.jsx";
-import ContactList from "./components/ContactList/ContactList.jsx";
-import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import { PiAddressBookDuotone } from "react-icons/pi";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]);
+
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts")) || [];
+    setContacts(savedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => [newContact, ...prevContacts]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    );
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <div className="phonebook-box">
-      <h1 className="phonebook-title">Phonebook</h1>
-
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-      <Contact />
+      <h1 className="phonebook-title">
+        Phonebook
+        <PiAddressBookDuotone className="icon" />
+      </h1>
+      <ContactForm onAddContact={addContact} />
+      <SearchBox value={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,1 +1,31 @@
 import s from "./ContactList.module.css";
+import PropTypes from "prop-types";
+import Contact from "../Contact/Contact";
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={s.list}>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <Contact
+            name={name}
+            number={number}
+            onDelete={() => onDeleteContact(id)}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
