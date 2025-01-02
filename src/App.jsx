5abcab535f7577310bda24808249c6d92cf93e4c
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-01-02 19:17:50 +0100

    remark

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,35 @@ import { PiAddressBookDuotone } from "react-icons/pi";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
-    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
-    return (
-      savedContacts || [
-        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-      ]
-    );
+    const savedContacts = window.localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
   });
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const [filter, setFilter] = useState("");
 
-  const saveToLocalStorage = (data) => {
-    localStorage.setItem("contacts", JSON.stringify(data));
-  };
-
   const addContact = (newContact) => {
-    const updatedContacts = [newContact, ...contacts];
-    setContacts(updatedContacts);
-    saveToLocalStorage(updatedContacts);
+    setContacts((prevContacts) => [newContact, ...prevContacts]);
   };
 
   const deleteContact = (id) => {
-    const updatedContacts = contacts.filter((contact) => contact.id !== id);
-    setContacts(updatedContacts);
-    saveToLocalStorage(updatedContacts);
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    );
   };
 
   const handleFilterChange = (e) => {
